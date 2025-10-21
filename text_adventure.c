d10f2dfed366bf1539190daa4f5713b374e115f0
BigJohnBos <boston.ballash@ucas-edu.net>
2025-10-21 14:41:58 -0600

    Co-authored-by: kalzcool <kalzcool@users.noreply.github.com> Co-authored-by: Phantomcat <Phatomcat@users.noreply.github.com>

--- text_adventure.c
+++ text_adventure.c
@@ -1,23 +1,31 @@
 // CC & BB & CP & MW final c project
 
 #include <stdio.h>
-
+#include <string.h>
 //CC
-void forward(void){
-    char yes;
-    char no;
+int forward(void){
+    char answer[5];
     printf("You walk forward onto the path. It brings you deeper into the forest\n");
-    printf("^You found a large stick on the path!^\n");
-    printf("^Would you like to keep it?^\n");
-    scanf("%s", &yes, &no);
+    printf("You found a large stick on the path!\n");
+    printf("Would you like to keep it? it might be helpful later\n");
+    scanf("%s", &answer);
 
+    if(strcmp(answer, "Yes")==0){
+        printf("[stick] Has been added tp your inventory^^");
+    }else if(strcmp(answer, "No")==0){
+        printf("You leave the stick on the trail.");
+    }else{
+        printf("Dude, yes or no please.");
+    }
+
+    
     return 0; 
 }
 
 
 
 //CP
-void right(void){
+int right(void){
     
 
 printf("while you are walking you trip on a rock and are injured\n");
@@ -30,12 +38,20 @@ return 0;
 }
     
 //mw
-void left(void){
-path_decision[10];
+int left(void){
+char path_decision[10];
 printf("You walk left on the path\n");
 printf("You see a house in the distance\n");
 printf("Do you go to the house or stay on the path?\n");
-scanf("%s", path_decision);
+scanf("%s", &path_decision);
+
+if (strcmp(path_decision, "stay")==0){
+    printf("a bear came and ate you");
+}else if (strcmp(path_decision, "go")==0){
+    printf("you go to the house and see a  ");
+}
+
+
 
 
     return 0;
@@ -53,29 +69,32 @@ scanf("%s", path_decision);
 
 //BB
 int main(void){
-    char path
     int bob = 1;
     char decision[10];
     char name[30];
+    char invintory[213][1000] = {"Pocket Knife", "Mirror"};
     printf("What is your name?\n");
-    scanf("%s\n", name);
+    scanf("%s", &name);
     printf("Welcome %s, to the forest! \n", name);
     printf("You wake up on a path in the middle of an unfamiliar forest\n");
+    printf("In your pocket you have %s", invintory);
     printf("You don't know how you got there but you see three paths in front of you, Forward, Left, or Right.\n");
-    printf("Which way would you like to go?\n"); //CC
-    scanf("%s\n", decision);
+
     while(bob == 1){
-        if(decision == "Forward"){
+        
+        printf("Which way would you like to go?\n");
+        scanf("%s", decision);
+        if(strcmp(decision, "Forward")==0){
             forward();
             bob = 0;
-        }else if(decision == "Left"){
+        }else if(strcmp(decision, "Left")==0){
             left();
             bob = 0;
-        }else if(decision == "Right"){
+        }else if(strcmp(decision,"Right")==0){
             right();
             bob = 0;
         }else{
-            printf("Type a valid answer\n")
+            printf("Type a valid answer\n");
         }
 
 
